c33ac584e9fd08b4b018d487fcdd4b0bf1492d5c
shenxianpeng <xianpeng.shen@gmail.com>
2025-07-23 02:05:51 +0300

    fix: not show explain error button when it disabled

--- src/main/java/io/jenkins/plugins/explain_error/ConsolePageDecorator.java
+++ src/main/java/io/jenkins/plugins/explain_error/ConsolePageDecorator.java
@@ -12,4 +12,9 @@ public class ConsolePageDecorator extends PageDecorator {
     public ConsolePageDecorator() {
         super();
     }
+
+    public boolean isExplainErrorEnabled() {
+        io.jenkins.plugins.explain_error.GlobalConfigurationImpl config = jenkins.model.Jenkins.get().getDescriptorByType(io.jenkins.plugins.explain_error.GlobalConfigurationImpl.class);
+        return config != null && config.isEnableExplanation();
+    }
 }
