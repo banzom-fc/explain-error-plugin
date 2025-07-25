ae1bc82a4dfceb811cc25866844dd5a6ada71e5f
shenxianpeng <xianpeng.shen@gmail.com>
2025-07-25 09:32:56 +0000

    fix: check apiKey is empty

--- src/main/java/io/jenkins/plugins/explain_error/ConsolePageDecorator.java
+++ src/main/java/io/jenkins/plugins/explain_error/ConsolePageDecorator.java
@@ -2,6 +2,7 @@ package io.jenkins.plugins.explain_error;
 
 import hudson.Extension;
 import hudson.model.PageDecorator;
+import hudson.util.Secret;
 
 /**
  * Page decorator to add "Explain Error" functionality to console output pages.
@@ -16,7 +17,7 @@ public class ConsolePageDecorator extends PageDecorator {
     public boolean isExplainErrorEnabled() {
         GlobalConfigurationImpl config = GlobalConfigurationImpl.get();
         return config.isEnableExplanation() && 
-               config.getApiKey() != null && 
+               !Secret.toString(config.getApiKey()).isBlank() && 
                config.getApiUrl() != null && !config.getApiUrl().trim().isEmpty();
     }
 }
